0088354711c76c9c598b13841c95bea4a54ba6ec
Alek Armbruster <alekarmbruster@gmail.com>
2018-04-13 13:16:30 +0200

    Update

--- getHistoryForDog.js
+++ getHistoryForDog.js
@@ -54,8 +54,8 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 	const request = {
 		//targets : --- letting this default to the peers assigned to the channel
 		chaincodeId: 'dogs',
-		fcn: 'queryAllDogs',
-		args: ['']
+		fcn: 'historyForDog',
+		args: ['DOG3']
 	};
 
 	// send the query proposal to the peer
